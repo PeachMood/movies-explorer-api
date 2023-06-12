fd748419481618bae561992c8d4776b27c8edbed
PeachMood <vas140302@gmail.com>
2023-06-13 00:37:24 +0700

    feat: добавить проверку авторизации

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const appConfig = require('./configs/appConfig');
 const dbConfig = require('./configs/dbConfig');
 
 const app = express();
+
+app.use(cookieParser());
 
 mongoose.connect(dbConfig.uri);
 
