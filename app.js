1899c76847be8b26c5cfd472bd5dc9c91b9c7a58
PeachMood <vas140302@gmail.com>
2023-06-13 21:18:49 +0700

    feat: добавить логирование ошибок и запросов

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const cookieParser = require('cookie-parser');
 const appConfig = require('./configs/appConfig');
 const dbConfig = require('./configs/dbConfig');
 const corsResolver = require('./middlewares/corsResolver');
+const requestLogger = require('./middlewares/loggers/requestLogger');
+const errorLogger = require('./middlewares/loggers/errorLogger');
 const errorHandler = require('./middlewares/errorHandler');
+const router = require('./routes');
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(corsResolver);
+app.use(requestLogger);
+app.use(router);
+app.use(errorLogger);
 app.use(errorHandler);
 
 mongoose.connect(dbConfig.uri);
