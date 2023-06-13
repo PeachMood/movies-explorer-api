6a2c1edd0c3b70af79b69c0ff10ffe5ef8368b09
PeachMood <vas140302@gmail.com>
2023-06-13 21:35:06 +0700

    feat: добавить средства для обеспечения безопасности

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const appConfig = require('./configs/appConfig');
 const dbConfig = require('./configs/dbConfig');
+const rateLimiter = require('./middlewares/rateLimiter');
 const corsResolver = require('./middlewares/corsResolver');
 const requestLogger = require('./middlewares/loggers/requestLogger');
 const errorLogger = require('./middlewares/loggers/errorLogger');
@@ -12,6 +14,8 @@ const router = require('./routes');
 
 const app = express();
 
+app.use(rateLimiter);
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(corsResolver);
